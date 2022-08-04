0790ccdbdc3f1695ec4ebb995875d03bd799ed33
rodri-ribes <rodri_ribes@hotmail.com>
2022-08-04 17:38:23 -0300

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import Footer from "./components/Footer/Footer.js";
 import './app.css'
 import Home from "./components/Home/Home.js";
 import Detail from "./components/BookDetail/Detail"
+import SignIn from './components/SignIn/SignIn.js'
+import SignUp from './components/SignUp/SignUp.js'
 
 function App() {
 
@@ -15,6 +17,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/:id" element={<Detail />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          {/* <Route path="/favorites" element={<Favorite />}/> */}
         </Routes>
       </div>
       <Footer />
---
+++ client/src/components/SignIn/SignIn.js
@@ -0,0 +1,115 @@
+import React, { useEffect, useState } from 'react'
+import style from './SignIn.module.css'
+
+import { Formik, Field, ErrorMessage, Form } from 'formik';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios'
+import { useDispatch, useSelector } from 'react-redux';
+import { getLibros } from '../../redux/features/data/dataSlice';
+
+
+export default function SignIn() {
+
+    const [confirm, setConfirm] = useState({
+        message: "",
+        visible: null,
+        error: null,
+    })
+
+    let navigate = useNavigate()
+    let dispatch = useDispatch();
+
+
+    // useEffect(() => {
+
+    //     if (window.localStorage.getItem("user")) {
+    //         setTimeout(() => {
+    //             navigate('/')
+    //         }, 100);
+    //     }
+
+    // }, [navigate])
+
+
+    return (
+        <Formik
+            initialValues={{
+                email: "",
+                password: "",
+            }}
+            onSubmit={async (valores, { resetForm }) => {
+
+                let { email, password } = valores;
+
+                try {
+                    let resp = await axios.post(`http://localhost:3001/signin`, {
+                        password, email
+                    })
+                    window.localStorage.setItem("user", JSON.stringify(resp.data))
+
+                    setConfirm({ message: "You logged in successfully", visible: true, error: false })
+
+                    setTimeout(() => {
+                        setConfirm({ message: "", visible: null, error: null })
+                        dispatch(getLibros())
+                        navigate("/")
+                    }, 2000);
+
+                    resetForm();
+
+                } catch (error) {
+
+                    setConfirm({ message: error.response.data, visible: true, error: true })
+                    setTimeout(() => {
+                        setConfirm({ message: "", visible: null, error: null })
+                    }, 2000);
+
+                }
+            }}
+            validate={(valores) => {
+                let errores = {};
+
+                if (!valores.email) {
+                    errores.email = "Enter email"
+                } else if (!/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(valores.email)) {
+                    errores.email = "Enter a valid email"
+                }
+                if (!valores.password) {
+                    errores.password = "Enter Password"
+                } else if (/^.{1,5}$/.test(valores.password)) {
+                    errores.password = "Enter a minimum of 6 characters"
+                }
+                return errores;
+            }}
+        >
+            {({ errors }) => (
+                <div className={style.ContainerForm}>
+                    <Form className={style.Container}>
+                        <h1 className={style.Container_Title}>SignIn</h1>
+                        <div className={style.Container__Div}>
+                            <Field className={style.Container__Div_Input}
+                                type="email"
+                                id="email"
+                                name="email"
+                                placeholder="Email"
+                            />
+                            <ErrorMessage name='email' component={() => (<div className={style.Container__Div_Error}><p>{errors.email}</p></div>)} />
+                        </div>
+                        <div className={style.Container__Div}>
+                            <Field className={style.Container__Div_Input}
+                                type="password"
+                                id="password"
+                                name="password"
+                                placeholder="Password"
+                            />
+                            <ErrorMessage name='password' component={() => (<div className={style.Container__Div_Error}><p>{errors.password}</p></div>)} />
+                        </div>
+                        <button type='submit' className={style.Container__Button}>SignIn</button>
+                        <p className={style.Container__Register}>You do not have an account? <Link to="/signup" className={style.Container__Register_Link}>Sign up</Link></p>
+                        {confirm.visible ? <div className={`${confirm.error ? style.Container__Div_NotSucess : style.Container__Div_Sucess}`}><p>{confirm.message}</p></div> : null}
+                    </Form>
+                </div>
+            )}
+        </Formik>
+    )
+}
