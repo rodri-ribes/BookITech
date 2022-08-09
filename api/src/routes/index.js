8e642070a1ca6ba1f2de82f37c5e2030d5fa794e
unknown <elgatoban@gmail.com>
2022-08-08 21:45:56 -0500

    Comments Routes

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,14 @@
 const { Router } = require('express');
 const booksRoutes = require ("./books")
 const usersRoutes = require ("./users")
+const commentsRoutes = require ("./comments")
 
 const router = Router()
 
 router.use("/books", booksRoutes)
 router.use("/signup", usersRoutes);
 router.use("/signin", usersRoutes)
+router.use("/comments",commentsRoutes)
 
 
 module.exports = router
