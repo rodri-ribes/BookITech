86db2e689505d19998e36b296e0779133a526731
pablodclavijo <76256398+pablodclavijo@users.noreply.github.com>
2022-08-09 23:23:15 -0300

    bugs detail + correcciones ingles + mayusculas

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -3,9 +3,10 @@ import CardBook from './CardBook/CardBook';
 import style from './home.module.css';
 import { Paginacion } from './Pagination/Pagination';
 import { useDispatch, useSelector } from 'react-redux';
-import { deleteBookDetail, getLibros } from '../../redux/features/data/dataSlice';
+import {  getLibros } from '../../redux/features/data/dataSlice';
 import Search from '../Search/Search';
 import Filters from '../Filters/Filters';
+import capitalize from '../auxiliar/capitalize'
 
 export default function Home() {
     let dispatch = useDispatch();
@@ -47,12 +48,8 @@ export default function Home() {
                     .map((l, i) => {
                         return (
                             <CardBook
-                                name={l.title}
+                                name={capitalize(l.title)}
                                 id={l.isbn13}
-                                author={l.author}
-                                gender={l.gender}
-                                idiom={l.idiom}
-                                format={l.format}
                                 price={l.price}
                                 img={l.image}
                                 key={i}
