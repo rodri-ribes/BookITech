cce09b90cc9594bd06a8b7e52155f82401863312
unknown <elgatoban@gmail.com>
2022-08-11 15:04:35 -0500

    merge

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,16 +1,4 @@
 const { Router } = require('express');
-<<<<<<< HEAD
-const booksRoutes = require ("./books")
-const usersRoutes = require ("./users")
-const commentsRoutes = require ("./comments")
-const cartRoutes = require('./cartRoutes');
-const router = Router()
-
-router.use("/books", booksRoutes);
-router.use("/signup", usersRoutes);
-router.use("/signin", usersRoutes)
-router.use("/comments",commentsRoutes)
-=======
 const booksRoutes = require("./books")
 const usersRoutes = require("./users")
 const commentsRoutes = require("./comments")
@@ -23,7 +11,6 @@ const router = Router()
 router.use("/books", booksRoutes);
 router.use("/", usersRoutes);
 router.use("/comments", commentsRoutes)
->>>>>>> 324d33f94f7b9d5e9afd05f2d86cf46ebf909e1d
 router.use('/cart', cartRoutes)
 router.use('/favorite', FavRouter)
 router.use('/save', saveSignInRoutes)
