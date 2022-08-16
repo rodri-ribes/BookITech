3a7c3a3b55c28a6ff61c6d49c47505abc014ed3e
Jorge Guazzini <jorge.guazzini.y@gmail.com>
2022-08-16 11:22:29 -0400

    make it better 5

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const server = require("./src/app")
 require('dotenv').config();
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3000
 
 server.listen(port, () => {
   console.log(' listening at', port)
