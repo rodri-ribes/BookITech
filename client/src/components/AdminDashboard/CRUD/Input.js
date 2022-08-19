453601baee3fadcce3e81e151eef3710342bddbd
pablodclavijo <76256398+pablodclavijo@users.noreply.github.com>
2022-08-19 14:43:07 -0300

    cleanup input file

--- client/src/components/AdminDashboard/CRUD/Input.js
+++ client/src/components/AdminDashboard/CRUD/Input.js
@@ -1,12 +1,10 @@
-import React, { useEffect } from "react"
+import React from "react"
 import { useDispatch } from "react-redux"
 import { TextField } from "@mui/material"
-import { getSearch } from '../../../redux/features/data/dataSlice';
 
 export function Input(props){
 
     const {inputCallback, value} = props
-    const dispatch = useDispatch()
     const handleOnChange = (e)=>{
       e.preventDefault()
       inputCallback(e.target.value)
