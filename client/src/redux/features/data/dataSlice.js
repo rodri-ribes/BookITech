59b557e4489661606649dcf6a21444b52c75f777
LuchoD99 <luciano.diazocampo@gmail.com>
2022-08-21 23:22:17 -0300

    cosas del nodemailer

--- client/src/redux/features/data/dataSlice.js
+++ client/src/redux/features/data/dataSlice.js
@@ -419,7 +419,7 @@ export const dataSlice = createSlice({
 
         heart:(state, actions)=>{
             state.heart= actions.payload;
-        }
+        },
 
         contadorCart: (state, actions) => {
             state.CartUser = actions.payload;
