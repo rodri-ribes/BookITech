c855fc4d522c1cadb8175194f3e3da65734138fb
Rodrigo Ribes <104105156+rodri-ribes@users.noreply.github.com>
2022-10-01 17:07:08 -0300

    first commit

--- api/src/database/db.js
+++ api/src/database/db.js
@@ -1,15 +1,8 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const { mongoose } = require('mongoose')
 require('dotenv').config();
-const { URI } = process.env
-// const client = new MongoClient(URI, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
 
-//   client.close();
 
-//});
 const uriHardCoded = "mongodb+srv://JGuazzini:pedroangel02@cluster0.y36ey.mongodb.net/?retryWrites=true&w=majority"
 mongoose
   .connect(uriHardCoded, { useNewUrlParser: true, useUnifiedTopology: true })
